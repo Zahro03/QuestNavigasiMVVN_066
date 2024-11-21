c34c786494a020899737e80dc2aa98b8dc3d5e5c
Zahro03 <zahrotul.jannah.ft22@mail.umy>
2024-11-21 18:30:53 +0700

    Sebuah data class yang menyimpan informasi pribadi seorang mahasiswa

---
+++ app/src/main/java/com/example/meet7/model/DataMahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.project5_mvvm.model
+
+data class DataMahasiswa(
+    val nama: String = " ",
+    val gender: String = " ",
+    val email: String =" ",
+    val alamat: String = " ",
+    val nim: String = " "
+)
